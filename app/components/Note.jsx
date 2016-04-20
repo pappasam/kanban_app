c579fcd5a78f398db5563e14f0b192cd9640248a
Sam <samuel.roeca@gmail.com>
2016-04-20 14:33:39 -0400

    Complete process up to 'Communicating Note state changes

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,50 @@
 import React from 'react';
 
-export default () => <div>Learn Webpack, homie</div>;
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false
+    };
+  }
+  render() {
+    if (this.state.editing) {
+      return this.renderEdit();
+    }
+    return this.renderNote();
+  }
+  renderEdit = () => {
+    return <input type="text"
+      ref={
+        element => element ?
+          element.selectionStart = this.props.task.length :
+          null
+      }
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />;
+  };
+  renderNote = () => {
+    return <div onClick={this.edit}>{this.props.task}</div>;
+  };
+  edit = () => {
+    this.setState({
+      editing: true
+    });
+  };
+  checkEnter = (e) => {
+    if (e.key === 'Enter') {
+      this.finishedEdit(e);
+    }
+  };
+  finishEdit = (e) => {
+    const value = e.target.value;
+    if (this.props.onEdit) {
+      this.props.onEdit(value);
+      this.setState({
+        editing: false
+      });
+    }
+  };
+}
