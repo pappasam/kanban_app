af25b3a7166b7923d13113627e4e939dfc143690
Sam Roeca <samuel.roeca@gmail.com>
2016-04-24 14:01:12 -0400

    Through Attaching Classes to Components

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,5 @@
 import uuid from 'node-uuid';
 import React from 'react';
-
 import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
@@ -32,10 +31,23 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <Notes notes={notes} />
+        <Notes
+          notes={notes}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote}
+          />
       </div>
     );
   }
+
+  deleteNote = (id, e) => {
+    // Avoid bubbling to edit
+    e.stopPropagation();
+
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== id)
+    });
+  };
 
   addNote = () => {
     this.setState({
@@ -44,5 +56,21 @@ export default class App extends React.Component {
         task: "New task"
       }])
     });
+  };
+
+  editNote = (id, task) => {
+    if (!task.trim()) {
+      return;
+    }
+
+    const notes = this.state.notes.map(note => {
+      if (note.id === id && task) {
+        note.task = task;
+      }
+
+      return note;
+    });
+
+    this.setState({notes});
   };
 }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
 export default class Note extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       editing: false
     };
   }
+
   render() {
     if (this.state.editing) {
       return this.renderEdit();
     }
     return this.renderNote();
   }
+
   renderEdit = () => {
     return <input type="text"
       ref={
@@ -25,19 +28,34 @@ export default class Note extends React.Component {
       onBlur={this.finishEdit}
       onKeyPress={this.checkEnter} />;
   };
+
   renderNote = () => {
-    return <div onClick={this.edit}>{this.props.task}</div>;
+    const onDelete = this.props.onDelete;
+
+    return (
+      <div onClick={this.edit}>
+        <span>{this.props.task}</span>
+        {onDelete ? this.renderDelete() : null}
+      </div>
+    );
   };
+
+  renderDelete = () => {
+    return <button onClick={this.props.onDelete}>x</button>;
+  };
+
   edit = () => {
     this.setState({
       editing: true
     });
   };
+
   checkEnter = (e) => {
     if (e.key === 'Enter') {
       this.finishedEdit(e);
     }
   };
+
   finishEdit = (e) => {
     const value = e.target.value;
     if (this.props.onEdit) {
