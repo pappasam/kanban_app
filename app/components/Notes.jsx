a0b3007086e90c56b779e329b97e83f3ba2440ad
Sam Roeca <samuel.roeca@gmail.com>
2016-05-30 21:30:34 -0400

    Revised something

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -4,14 +4,14 @@ import Note from './Note.jsx';
 export default ({notes, onEdit, onDelete}) => {
   return (
     <ul className="notes">{notes.map(note =>
-                 <li className="note" key={note.id}>
-                   <Note
-                     task={note.task}
-                     onEdit={onEdit.bind(null, note.id)}
-                     onDelete={onDelete.bind(null, note.id)}
-                     />
-                 </li>
-                )}
+      <li className="note" key={note.id}>
+        <Note
+        task={note.task}
+        onEdit={onEdit.bind(null, note.id)}
+        onDelete={onDelete.bind(null, note.id)}
+        />
+      </li>
+    )}
     </ul>
   );
 }
